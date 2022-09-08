aadaa006ea2b78728496d06013364fe3cfa4ba5b
James Cuénod <jcuenod@users.noreply.github.com>
2022-09-08 10:21:37 -0500

    Test commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { Database } from "https://deno.land/x/sqlite3@0.5.2/mod.ts";
+
+const db = new Database("./build/parabible.sqlite");
+
+const [version] = db.prepare("select sqlite_version()").get<[string]>()!;
+console.log(version);
+
+db.close();
